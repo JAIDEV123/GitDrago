4bf0be66647d1c13096da9e0dcc68c12429ea3c9
Jaidev Shriram <jaidevtrumpet@gmail.com>
2019-12-09 00:25:27 +0530

    Minor Update, Standard Message

--- libdrac.py
+++ libdrac.py
@@ -37,13 +37,18 @@ class GitRepository(object):
     """A git repository"""
 
     worktree = None
+   
+    # This would be the directory path to .git
     gitdir = None
+
+    # This variable will be the configuration file contents
     conf = None
 
     def __init__(self, path, force=False):
         self.worktree = path
         self.gitdir = os.path.join(path, ".git")
 
+        # If we aren't making it a git repo or if the .git dir doesn't exist, raise error
         if not (force or os.path.isdir(self.gitdir)):
             raise Exception("Not a Git repository %s" % path)
 
@@ -55,7 +60,7 @@ class GitRepository(object):
                 self.conf.read([config_file])
         elif not force:
             raise Exception("Configuration file missing")
-
+    
         if not force:
             vers = int(self.conf.get("core", "repositoryformatversion"))
             if vers != 0:
@@ -67,8 +72,8 @@ def repo_path(repo, *path):
 
 def repo_file(repo, *path, mkdir=False):
     """Same as repo_path, but create dirname(*path) if absent.  For
-example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create
-.git/refs/remotes/origin."""
+    example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create
+    .git/refs/remotes/origin."""
 
     if repo_dir(repo, *path[:-1], mkdir=mkdir):
         return repo_path(repo, *path)
@@ -89,3 +94,5 @@ def repo_dir(repo, *path, mkdir=False):
         return path
     else:
         return None
+
+    
