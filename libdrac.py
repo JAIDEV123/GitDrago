f077cea83f1fd7de381cc49c95002bf9e4ab0d7e
Jaidev Shriram <jaidevtrumpet@gmail.com>
2019-12-09 01:07:35 +0530

    Done with git init

--- libdrac.py
+++ libdrac.py
@@ -7,7 +7,8 @@ import re
 import sys
 import zlib
 
-argparser = argparse.ArgumentParser(description="Routes git COMMAND options to the corresponding command")
+argparser = argparse.ArgumentParser(description="Version Control")
+
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
 
@@ -29,41 +30,32 @@ def main(argv=sys.argv[1:]):
     elif args.command == "show-ref"    : cmd_show_ref(args)
     elif args.command == "tag"         : cmd_tag(args)
 
-'''
-REPOSITORY FUNCTIONS
-'''
-
 class GitRepository(object):
     """A git repository"""
 
     worktree = None
-   
-    # This would be the directory path to .git
     gitdir = None
-
-    # This variable will be the configuration file contents
     conf = None
 
     def __init__(self, path, force=False):
         self.worktree = path
         self.gitdir = os.path.join(path, ".git")
 
-        # If we aren't making it a git repo or if the .git dir doesn't exist, raise error
         if not (force or os.path.isdir(self.gitdir)):
             raise Exception("Not a Git repository %s" % path)
 
         # Read configuration file in .git/config
         self.conf = configparser.ConfigParser()
-        config_file = repo_file(self, "config")
+        cf = repo_file(self, "config")
 
-        if config_file and os.path.exists(config_file):
-                self.conf.read([config_file])
+        if cf and os.path.exists(cf):
+                self.conf.read([cf])
         elif not force:
             raise Exception("Configuration file missing")
-    
+
         if not force:
             vers = int(self.conf.get("core", "repositoryformatversion"))
-            if vers != 0:
+            if vers != 0 and not force:
                 raise Exception("Unsupported repositoryformatversion %s" % vers)
 
 def repo_path(repo, *path):
@@ -72,8 +64,8 @@ def repo_path(repo, *path):
 
 def repo_file(repo, *path, mkdir=False):
     """Same as repo_path, but create dirname(*path) if absent.  For
-    example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create
-    .git/refs/remotes/origin."""
+example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create
+.git/refs/remotes/origin."""
 
     if repo_dir(repo, *path[:-1], mkdir=mkdir):
         return repo_path(repo, *path)
@@ -95,4 +87,58 @@ def repo_dir(repo, *path, mkdir=False):
     else:
         return None
 
-    
+def repo_create(path):
+    """Create a new repository at path."""
+
+    repo = GitRepository(path, True)
+
+    # First, we make sure the path either doesn't exist or is an
+    # empty dir.
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception ("%s is not a directory!" % path)
+        if os.listdir(repo.worktree):
+            raise Exception("%s is not empty!" % path)
+    else:
+        os.makedirs(repo.worktree)
+
+    assert(repo_dir(repo, "branches", mkdir=True))
+    assert(repo_dir(repo, "objects", mkdir=True))
+    assert(repo_dir(repo, "refs", "tags", mkdir=True))
+    assert(repo_dir(repo, "refs", "heads", mkdir=True))
+
+    # .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository; edit this file 'description' to name the repository.\n")
+
+    # .git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
+
+argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
+
+argsp.add_argument("path",
+                   metavar="directory",
+                   nargs="?",
+                   default=".",
+                   help="Where to create the repository.")
+
+def cmd_init(args):
+    repo_create(args.path)
